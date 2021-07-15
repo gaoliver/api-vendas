9fef64f68403df0986e06189f4a01fc1d3c55c07
gaoliver <gabriel.o.c.ramos@gmail.com>
2021-07-14 22:19:54 -0300

    feat: Service para Update de Produto

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '@shared/errors/AppError';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+export default class UpdateProductService {
+    public async execute({
+        id,
+        name,
+        price,
+        quantity,
+    }: Product): Promise<Product> {
+        const productsRepository = getCustomRepository(ProductRepository);
+
+        const product = await productsRepository.findOne(id);
+
+        if (!product) {
+            throw new AppError('Product not found');
+        }
+
+        const productExists = await productsRepository.findByName(name);
+
+        if (productExists) {
+            throw new AppError('There is already a product with this name');
+        }
+
+        product.name = name;
+        product.price = price;
+        product.quantity = quantity;
+
+        return product;
+    }
+}
