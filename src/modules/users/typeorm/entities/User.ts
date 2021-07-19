ccd97d49c894ed7e2d3eeebce73dd5bf2ac76e43
gaoliver <gabriel.o.c.ramos@gmail.com>
2021-07-19 11:03:51 -0300

    feat: Entidade de usuário

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,33 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    avatar: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
+
+export default User;
