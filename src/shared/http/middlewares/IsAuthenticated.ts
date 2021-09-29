2c0d2a8d35d20774dcced2889462820555eb7744
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-09-29 08:59:32 -0300

    feat: Nova pasta de sobretipagem + importação de informações de autenticação do usuário

--- src/shared/http/middlewares/IsAuthenticated.ts
+++ src/shared/http/middlewares/IsAuthenticated.ts
@@ -3,6 +3,12 @@ import AppError from '@shared/errors/AppError';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
 export default function IsAuthenticated(
     request: Request,
     response: Response,
@@ -17,7 +23,13 @@ export default function IsAuthenticated(
     const [, token] = authHeader.split(' ');
 
     try {
-        const decodeToken = verify(token, auth.jwt.secret);
+        const decodedToken = verify(token, auth.jwt.secret);
+
+        const { sub } = decodedToken as TokenPayload;
+
+        request.user = {
+            id: sub,
+        };
 
         return next();
     } catch {
