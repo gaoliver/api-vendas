831f7b5580b0e6e593991908b22a16cef29afe4a
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-11 10:30:11 -0300

    feat: Serviço reset de senha

---
+++ src/modules/users/services/ResetPasswdService.ts
@@ -0,0 +1,41 @@
+import { getCustomRepository } from 'typeorm';
+import { addHours } from 'date-fns/esm';
+import { isAfter } from 'date-fns';
+import { hash } from 'bcrypt';
+
+import AppError from '@shared/errors/AppError';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+    token: string;
+    password: string;
+}
+
+export default class ResetPasswdService {
+    public async execute({ token, password }: IRequest): Promise<void> {
+        const usersRepository = getCustomRepository(UsersRepository);
+        const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+        const userToken = await userTokensRepository.findByToken(token);
+
+        if (!userToken) {
+            throw new AppError('User Token does not exist.');
+        }
+
+        const user = await usersRepository.findById(userToken.user_id);
+
+        if (!user) {
+            throw new AppError('User does not exist.');
+        }
+
+        const tokenCreatedAt = userToken.createdAt;
+        const compareDate = addHours(tokenCreatedAt, 2);
+
+        if (isAfter(Date.now(), compareDate)) {
+            throw new AppError('Token expired.');
+        }
+
+        user.password = await hash(password, 8);
+    }
+}
