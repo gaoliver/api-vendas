09666a3ebd5aa7f56a8972a44f078660713aa1a0
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-11 09:57:19 -0300

    feat: Criação da Entity UserToken feat: Recriação da tabela user_tokens

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,29 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    Generated,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user_tokens')
+class UserToken {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    @Generated('uuid')
+    token: string;
+
+    @Column()
+    user_id: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
+
+export default UserToken;
