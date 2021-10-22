9d14e3ef813c81ba4f91fcce5f7eb73decc4e33f
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-22 08:58:35 -0300

    feat: Customers routes implemented

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import customersRouter from '@modules/customers/routes/customers.routes';
 
 const routes = Router();
 
@@ -15,5 +16,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/login', sessionRouter);
 routes.use('/password', passwordRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
