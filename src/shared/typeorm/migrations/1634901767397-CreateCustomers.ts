b88c848b2e1669bb1e972baca251b42b14ec19e0
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-22 08:25:45 -0300

    feat: Customers migration created

---
+++ src/shared/typeorm/migrations/1634901767397-CreateCustomers.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCustomers1634901767397 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'customers',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'createdAt',
+                        type: 'timestamp with time zone',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updatedAt',
+                        type: 'timestamp with time zone',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('customers');
+    }
+}
